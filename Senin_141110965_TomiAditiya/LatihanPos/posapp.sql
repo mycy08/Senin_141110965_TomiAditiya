e81b476dad05e11f3be2478c1feddaafe85cd371
Tommy Aditya <tommyaditya0606@gmail.com>
2016-12-26 19:59:10 +0700

    Mysql Updated

--- Senin_141110965_TomiAditiya/LatihanPos/posapp.sql
+++ Senin_141110965_TomiAditiya/LatihanPos/posapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 21 Des 2016 pada 07.19
+-- Generation Time: 26 Des 2016 pada 13.56
 -- Versi Server: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -49,10 +49,11 @@ INSERT INTO `account` (`id`, `username`, `password`, `level`) VALUES
 CREATE TABLE `tblbarang` (
   `idbarang` int(11) NOT NULL,
   `kodeBarang` varchar(20) NOT NULL,
+  `kodesuplier` varchar(50) NOT NULL,
   `namaBarang` varchar(100) NOT NULL,
   `jlhAwal` int(10) NOT NULL,
-  `hrgHPP` decimal(16,2) NOT NULL,
-  `hrgJual` decimal(16,2) NOT NULL,
+  `hrgHPP` int(11) NOT NULL,
+  `hrgJual` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -61,20 +62,9 @@ CREATE TABLE `tblbarang` (
 -- Dumping data untuk tabel `tblbarang`
 --
 
-INSERT INTO `tblbarang` (`idbarang`, `kodeBarang`, `namaBarang`, `jlhAwal`, `hrgHPP`, `hrgJual`, `created_at`, `update_at`) VALUES
-(3, '003', 'Soto Ayam', 2, '2000.00', '3000.00', '2016-12-14 11:04:30', '2016-12-14 11:04:30'),
-(4, '004', 'Merica', 200, '500.00', '1000.00', '2016-12-14 11:07:12', '2016-12-14 11:07:12'),
-(5, '005', 'Kecap Bango Botol', 5, '1200.00', '2500.00', '2016-12-14 11:09:16', '2016-12-21 02:18:31'),
-(6, '006', 'Kecap Asin', 23, '1000.00', '1500.00', '2016-12-14 11:10:03', '2016-12-14 11:10:03'),
-(7, '007', 'Kecap ABC', 26, '2500.00', '3000.00', '2016-12-14 11:12:09', '2016-12-14 11:12:09'),
-(8, '008', 'Teh Bendera', 50, '2500.00', '3000.00', '2016-12-14 11:22:25', '2016-12-14 11:22:25'),
-(11, '010', 'ayam', 5, '50000.00', '60000.00', '2016-12-14 13:44:37', '2016-12-14 13:44:37'),
-(12, '012', 'Gula', 45, '45.00', '45.00', '2016-12-14 13:49:27', '2016-12-21 02:19:42'),
-(13, '013', 'asd', 455, '455.00', '4555.00', '2016-12-14 13:51:13', '2016-12-14 13:51:13'),
-(14, '0015', 'asd', 500, '500.00', '500.00', '2016-12-14 13:53:55', '2016-12-14 13:53:55'),
-(15, '016', 'asd', 56, '56.00', '56.00', '2016-12-14 13:54:31', '2016-12-14 13:54:31'),
-(16, '0015', 'Susu Milk', 5, '2000.00', '5000.00', '2016-12-20 15:24:13', '2016-12-20 15:24:13'),
-(17, '0020', 'cendol', 150, '1500.00', '2000.00', '2016-12-20 15:30:22', '2016-12-20 15:30:22');
+INSERT INTO `tblbarang` (`idbarang`, `kodeBarang`, `kodesuplier`, `namaBarang`, `jlhAwal`, `hrgHPP`, `hrgJual`, `created_at`, `update_at`) VALUES
+(28, '001', '001', 'Pepsodent', 10, 2500, 5000, '2016-12-26 19:00:10', '2016-12-26 19:00:10'),
+(29, '002', '001', 'Citato', 5, 5000, 6500, '2016-12-26 19:00:31', '2016-12-26 19:00:31');
 
 -- --------------------------------------------------------
 
@@ -98,7 +88,128 @@ CREATE TABLE `tblcustomer` (
 --
 
 INSERT INTO `tblcustomer` (`idcust`, `nama`, `alamat`, `kodepos`, `email`, `nohp`, `created_at`, `updated_at`) VALUES
-(2, 'Tommy', 'Jl Mana saja', '20132', 'tommyaditya0606@gmail.com', '085206925484', '2016-12-21 13:14:51', '2016-12-21 13:14:51');
+(3, 'Indah', 'Jl Sukabumi ini', '25001', 'indah@gmail.com', '085206925484', '2016-12-26 19:03:09', '2016-12-26 19:15:15');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblpembelian`
+--
+
+CREATE TABLE `tblpembelian` (
+  `no` int(11) NOT NULL,
+  `idpembelian` int(11) NOT NULL,
+  `kodesuplier` varchar(50) NOT NULL,
+  `nm_perusahaan` varchar(50) NOT NULL,
+  `kodeBarang` varchar(20) NOT NULL,
+  `namaBarang` varchar(100) NOT NULL,
+  `hargaHPP` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblpenjualan`
+--
+
+CREATE TABLE `tblpenjualan` (
+  `idpenj` int(11) NOT NULL,
+  `no` int(11) NOT NULL,
+  `kodeBarang` varchar(20) NOT NULL,
+  `namaBarang` varchar(100) NOT NULL,
+  `hrgJual` int(11) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `subtotal` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblsuplier`
+--
+
+CREATE TABLE `tblsuplier` (
+  `idsuplier` int(50) NOT NULL,
+  `kodesuplier` varchar(50) NOT NULL,
+  `nm_perusahaan` varchar(50) NOT NULL,
+  `nama_contact` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `kodepos` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nohp` varchar(13) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblsuplier`
+--
+
+INSERT INTO `tblsuplier` (`idsuplier`, `kodesuplier`, `nm_perusahaan`, `nama_contact`, `alamat`, `kodepos`, `email`, `nohp`, `created_at`, `updated_at`) VALUES
+(2, '001', 'PT Suka aku lha', 'Andre', 'Jl Apa Aja', '50000', 'ada@gmail.com', '085206925484', '2016-12-23 15:44:31', '2016-12-26 18:54:48'),
+(4, '002', 'PT Cinta Kasih', 'Satrianjaya', 'Jl Kuoje', '2000', 'aku@gmail.com', '085206925484', '2016-12-26 19:02:05', '2016-12-26 19:02:05');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbltransaksipembelian`
+--
+
+CREATE TABLE `tbltransaksipembelian` (
+  `idtransaksipembelian` int(11) NOT NULL,
+  `faktur_pembelian` varchar(50) NOT NULL,
+  `kodesuplier` varchar(50) NOT NULL,
+  `nm_perusahaan` varchar(50) NOT NULL,
+  `kodeBarang` varchar(20) NOT NULL,
+  `namaBarang` varchar(100) NOT NULL,
+  `hargaHPP` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tglpembelian` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbltransaksipembelian`
+--
+
+INSERT INTO `tbltransaksipembelian` (`idtransaksipembelian`, `faktur_pembelian`, `kodesuplier`, `nm_perusahaan`, `kodeBarang`, `namaBarang`, `hargaHPP`, `jumlah`, `total`, `tglpembelian`, `updated_at`) VALUES
+(4, '2', '001', 'PT Suka aku lha', '0032', 'Cendol', 5000, 5, 25000, '2016-12-26 18:46:28', '2016-12-26 18:46:28'),
+(5, '3', '001', 'PT Suka aku lha', '0032', 'Cendol', 5000, 5, 25000, '2016-12-26 18:47:38', '2016-12-26 18:47:38'),
+(6, '4', '001', 'PT Suka aku lha', '001', 'Pepsodent', 2500, 3, 7500, '2016-12-26 19:21:38', '2016-12-26 19:21:38'),
+(7, '4', '001', 'PT Suka aku lha', '002', 'Citato', 5000, 3, 15000, '2016-12-26 19:21:38', '2016-12-26 19:21:38'),
+(8, '5', '001', 'PT Suka aku lha', '001', 'Pepsodent', 2500, 5, 12500, '2016-12-26 19:33:18', '2016-12-26 19:33:18');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbltransaksipenjualan`
+--
+
+CREATE TABLE `tbltransaksipenjualan` (
+  `idtransaksi` int(50) NOT NULL,
+  `faktur_penjualan` varchar(50) NOT NULL,
+  `idcust` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kodeBarang` varchar(20) NOT NULL,
+  `namaBarang` varchar(100) NOT NULL,
+  `hrgJual` int(11) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tglpenjualan` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbltransaksipenjualan`
+--
+
+INSERT INTO `tbltransaksipenjualan` (`idtransaksi`, `faktur_penjualan`, `idcust`, `nama`, `kodeBarang`, `namaBarang`, `hrgJual`, `jumlah`, `total`, `tglpenjualan`, `updated_at`, `status`) VALUES
+(49, '0', 3, 'Indah', '001', 'Pepsodent', 5000, 3, 15000, '2016-12-26 19:19:01', '2016-12-26 19:19:01', 'Lunas'),
+(50, '0', 3, 'Indah', '002', 'Citato', 6500, 3, 19500, '2016-12-26 19:19:01', '2016-12-26 19:19:01', 'Lunas');
 
 --
 -- Indexes for dumped tables
@@ -123,6 +234,36 @@ ALTER TABLE `tblcustomer`
   ADD PRIMARY KEY (`idcust`);
 
 --
+-- Indexes for table `tblpembelian`
+--
+ALTER TABLE `tblpembelian`
+  ADD PRIMARY KEY (`idpembelian`);
+
+--
+-- Indexes for table `tblpenjualan`
+--
+ALTER TABLE `tblpenjualan`
+  ADD PRIMARY KEY (`idpenj`);
+
+--
+-- Indexes for table `tblsuplier`
+--
+ALTER TABLE `tblsuplier`
+  ADD PRIMARY KEY (`idsuplier`);
+
+--
+-- Indexes for table `tbltransaksipembelian`
+--
+ALTER TABLE `tbltransaksipembelian`
+  ADD PRIMARY KEY (`idtransaksipembelian`);
+
+--
+-- Indexes for table `tbltransaksipenjualan`
+--
+ALTER TABLE `tbltransaksipenjualan`
+  ADD PRIMARY KEY (`idtransaksi`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -135,12 +276,37 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT for table `tblbarang`
 --
 ALTER TABLE `tblbarang`
-  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `tblcustomer`
 --
 ALTER TABLE `tblcustomer`
   MODIFY `idcust` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tblpembelian`
+--
+ALTER TABLE `tblpembelian`
+  MODIFY `idpembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `tblpenjualan`
+--
+ALTER TABLE `tblpenjualan`
+  MODIFY `idpenj` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tblsuplier`
+--
+ALTER TABLE `tblsuplier`
+  MODIFY `idsuplier` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tbltransaksipembelian`
+--
+ALTER TABLE `tbltransaksipembelian`
+  MODIFY `idtransaksipembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `tbltransaksipenjualan`
+--
+ALTER TABLE `tbltransaksipenjualan`
+  MODIFY `idtransaksi` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
